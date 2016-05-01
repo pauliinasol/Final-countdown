57d0232d6368db285e76baa36ac31eb11a96a9df
Pauliina Solanne <pauliinasolanne@Pauliina-MacBook-Air.local>
2016-05-02 08:50:17 +1000

    remember bundler

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # require 'httparty'
 require 'sinatra'
+
+include 'httparty'
 # require 'sinatra/reloader'
-
-
 
 get '/' do
    @melbourne = HTTParty.get('http://api.openweathermap.org/data/2.5/find?q=Melbourne&units=metric&appid=f01c8b50c27334c34b26a1f56ce2d5f5')
