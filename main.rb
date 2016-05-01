5dbcba97d74f1fe70290a49382091271159af279
Pauliina Solanne <pauliinasolanne@Pauliina-Air.lan>
2016-05-01 17:16:34 +1000

    lock

--- main.rb
+++ main.rb
@@ -5,9 +5,9 @@ require 'sinatra/reloader'
 
 
 get '/' do
-   @melbourne = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=melbourne&apikey=f01c8b50c27334c34b26a1f56ce2d5f5')
-   @melbourne_weather = @melbourne["main"]["temp"] - 273.5
-   @helsinki = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=helsinki&apikey=f01c8b50c27334c34b26a1f56ce2d5f5')
-   @helsinki_weather = @helsinki["main"]["temp"] - 273.5
+   @melbourne = HTTParty.get('http://api.openweathermap.org/data/2.5/find?q=Melbourne&units=metric&appid=f01c8b50c27334c34b26a1f56ce2d5f5')
+   @melbourne_weather = @melbourne["list"][0]["main"]["temp"]
+   @helsinki = HTTParty.get('http://api.openweathermap.org/data/2.5/find?q=Helsinki&units=metric&appid=f01c8b50c27334c34b26a1f56ce2d5f5')
+   @helsinki_weather = @helsinki["list"][0]["main"]["temp"]
    erb :index
  end
